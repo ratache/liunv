ca575864ec4a30bb1fb9bfe1ae1f8ee994e7c4f6
Per Johansson <ratache@gmail.com>
2014-11-26 12:13:18 +0100

    FIRST EDIT: NO ACTUAL CODE IS IMPLEMENTED YET. 	PER J

---
+++ src/junittesting/JUnitTesting.java
@@ -0,0 +1,20 @@
+/*
+    Testa JUnit
+    Per Johansson - tdd inl1
+ */
+package junittesting;
+
+/**
+ *
+ * @author UndeadAmilo
+ */
+public class JUnitTesting {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
