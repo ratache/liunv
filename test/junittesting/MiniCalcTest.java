2a7778cba6b51736c297c608e6ba5ae5b7bef472
Per Johansson <ratache@gmail.com>
2014-12-08 14:13:24 +0100

    RED! Added MiniCalc class. Added add method. Wrote a test(WRONG test should be written first but this is my first ever try!).(The test is probably wrong)

--- test/junittesting/MiniCalcTest.java
+++ test/junittesting/MiniCalcTest.java
@@ -5,10 +5,6 @@
  */
 package junittesting;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -20,44 +16,19 @@ public class MiniCalcTest {
     
     public MiniCalcTest() {
     }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
 
-    @Test
-    public void testSomeMethod() {
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-    
-    /*
-        Test of addition of two int values. Testing this addition method
-        is related to constraints on the input and checking that the output
-        is correct.
-    */
     @Test
     public void testadd() {
         try{
-            new MiniCalc();
-            fail();
+            MiniCalc bob = new MiniCalc();
+            int x1 = 1;
+            int x2 = -5;
+            bob.add(x1, x2);
+           //fail();
         }
         catch(IllegalArgumentException e){
             //ok!
         }
-        fail("The test case is a prototype.");
     }
     
 }
