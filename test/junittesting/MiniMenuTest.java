44a6d58142b707e0ea7c4417f7ea46a71917f34f
Per Johansson <ratache@gmail.com>
2015-01-04 14:25:30 +0100

    RED. Attempt at starting to mock by implementing the menu of the program TDD. The menu will be consolebased. We will test it by mocking the MiniCalc.

--- test/junittesting/MiniMenuTest.java
+++ test/junittesting/MiniMenuTest.java
@@ -11,34 +11,40 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
 
 /**
  *
- * @author UndeadAmilo
+ * @author Per Johansson
+ * this test follows a guide/book called Instant Mockito by
+ * Grzejszczak, Marcin. 
+ * Notes about procedure of testing with this framework
+ * For each defined test method:
+    * 1.define how your mock should behave(given)
+    * 2.execute the code(when)
+    * 3.verify the behaviour on the mock (then) 
  */
 public class MiniMenuTest {
+    public MiniMenu bobMenu;
+    private MiniCalc bobMock;
     
     public MiniMenuTest() {
+        bobMock = mock(MiniCalc.class);
     }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
+ 
     @Before
     public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
+        bobMenu = new MiniMenu();
     }
 
     @Test
-    public void testSomeMethod() {
+    public void testCorrectlyMenuChoice() throws Exception {
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+    @Test
+    public void testIncorrectlyMenuChoice()throws Exception {
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
